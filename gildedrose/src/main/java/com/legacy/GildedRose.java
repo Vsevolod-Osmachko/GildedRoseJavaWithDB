93e1e6cab937a3939cae24856c4df801e9a4a9a9
Vsevolod Osmachko <vsevolod_osmachko@epam.com>
2017-12-06 11:36:01 +0200

    Add db call

--- gildedrose/src/main/java/com/legacy/GildedRose.java
+++ gildedrose/src/main/java/com/legacy/GildedRose.java
@@ -1,6 +1,15 @@
 package com.legacy;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class GildedRose {
+
+    private Connection connection;
+
+    PreparedStatement statement = null;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -14,22 +23,149 @@ public class GildedRose {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
+
+                        statement = null;
+                        try {
+                            statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                            statement.setInt(1, items[i].quality);
+                            statement.setString(2, items[i].name);
+                            statement.executeUpdate();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                            if (connection != null) {
+                                try {
+                                    connection.rollback();
+                                } catch (SQLException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        } finally {
+                            if (statement != null) {
+                                try {
+                                    statement.close();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
                     }
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
+                    statement = null;
+                    try {
+                        statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                        statement.setInt(1, items[i].quality);
+                        statement.setString(2, items[i].name);
+                        statement.executeUpdate();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        if (connection != null) {
+                            try {
+                                connection.rollback();
+                            } catch (SQLException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    } finally {
+                        if (statement != null) {
+                            try {
+                                statement.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
+
+                                statement = null;
+                                try {
+                                    statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                                    statement.setInt(1, items[i].quality);
+                                    statement.setString(2, items[i].name);
+                                    statement.executeUpdate();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                    if (connection != null) {
+                                        try {
+                                            connection.rollback();
+                                        } catch (SQLException ex) {
+                                            ex.printStackTrace();
+                                        }
+                                    }
+                                } finally {
+                                    if (statement != null) {
+                                        try {
+                                            statement.close();
+                                        } catch (SQLException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+
+
+                                PreparedStatement statement = null;
+                                try {
+                                    statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                                    statement.setInt(1, items[i].quality);
+                                    statement.setString(2, items[i].name);
+                                    statement.executeUpdate();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                    if (connection != null) {
+                                        try {
+                                            connection.rollback();
+                                        } catch (SQLException ex) {
+                                            ex.printStackTrace();
+                                        }
+                                    }
+                                } finally {
+                                    if (statement != null) {
+                                        try {
+                                            statement.close();
+                                        } catch (SQLException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
+
+                                statement = null;
+                                try {
+                                    statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                                    statement.setInt(1, items[i].quality);
+                                    statement.setString(2, items[i].name);
+                                    statement.executeUpdate();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                    if (connection != null) {
+                                        try {
+                                            connection.rollback();
+                                        } catch (SQLException ex) {
+                                            ex.printStackTrace();
+                                        }
+                                    }
+                                } finally {
+                                    if (statement != null) {
+                                        try {
+                                            statement.close();
+                                        } catch (SQLException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+
                             }
                         }
                     }
@@ -38,6 +174,32 @@ public class GildedRose {
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
+
+                statement = null;
+                try {
+                    statement = connection.prepareStatement("update store set sellIn = ? where name = ?");
+                    statement.setInt(1, items[i].sellIn);
+                    statement.setString(2, items[i].name);
+                    statement.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    if (connection != null) {
+                        try {
+                            connection.rollback();
+                        } catch (SQLException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                } finally {
+                    if (statement != null) {
+                        try {
+                            statement.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
             }
 
             if (items[i].sellIn < 0) {
@@ -46,17 +208,99 @@ public class GildedRose {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
+
+                                statement = null;
+                                try {
+                                    statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                                    statement.setInt(1, items[i].quality);
+                                    statement.setString(2, items[i].name);
+                                    statement.executeUpdate();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                    if (connection != null) {
+                                        try {
+                                            connection.rollback();
+                                        } catch (SQLException ex) {
+                                            ex.printStackTrace();
+                                        }
+                                    }
+                                } finally {
+                                    if (statement != null) {
+                                        try {
+                                            statement.close();
+                                        } catch (SQLException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+
                             }
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
+
+                        statement = null;
+                        try {
+                            statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                            statement.setInt(1, items[i].quality);
+                            statement.setString(2, items[i].name);
+                            statement.executeUpdate();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                            if (connection != null) {
+                                try {
+                                    connection.rollback();
+                                } catch (SQLException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        } finally {
+                            if (statement != null) {
+                                try {
+                                    statement.close();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
                     }
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+
+                        statement = null;
+                        try {
+                            statement = connection.prepareStatement("update store set quality = ? where name = ?");
+                            statement.setInt(1, items[i].quality);
+                            statement.setString(2, items[i].name);
+                            statement.executeUpdate();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                            if (connection != null) {
+                                try {
+                                    connection.rollback();
+                                } catch (SQLException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        } finally {
+                            if (statement != null) {
+                                try {
+                                    statement.close();
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
                     }
                 }
             }
         }
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
     }
 }
