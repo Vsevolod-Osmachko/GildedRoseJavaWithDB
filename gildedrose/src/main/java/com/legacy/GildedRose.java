10f29161f36c6441a21b84eaa821a1422249d47b
Vsevolod Osmachko <vsevolod_osmachko@epam.com>
2019-11-16 08:25:41 +0200

    Extract

--- gildedrose/src/main/java/com/legacy/GildedRose.java
+++ gildedrose/src/main/java/com/legacy/GildedRose.java
@@ -30,23 +30,11 @@ public class GildedRose {
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-
-                                itemDao.saveQuality(items[i]);
-
-
-                                itemDao.saveQuality(items[i]);
-                            }
+                            increase(items[i]);
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-
-                                itemDao.saveQuality(items[i]);
-
-                            }
+                            increase(items[i]);
                         }
                     }
                 }
@@ -77,14 +65,17 @@ public class GildedRose {
 
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-
-                        itemDao.saveQuality(items[i]);
-
-                    }
+                    increase(items[i]);
                 }
             }
+        }
+    }
+
+    private void increase(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            itemDao.saveQuality(item);
         }
     }
 
