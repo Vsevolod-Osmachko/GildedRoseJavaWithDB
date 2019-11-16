1a36165de3486e691f3285aa668ead844c79705f
Vsevolod Osmachko <vsevolod_osmachko@epam.com>
2019-11-16 08:28:42 +0200

    Move condition

--- gildedrose/src/main/java/com/legacy/GildedRose.java
+++ gildedrose/src/main/java/com/legacy/GildedRose.java
@@ -15,10 +15,8 @@ public class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decrease(i);
-                    }
+                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decrease(i);
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -48,11 +46,9 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decrease(i);
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            decrease(i);
 
-                            }
                         }
                     } else {
                         items[i].quality = 0;
@@ -68,7 +64,8 @@ public class GildedRose {
     }
 
     private void decrease(int i) {
-        items[i].quality = items[i].quality - 1;
+        if (items[i].quality > 0)
+            items[i].quality = items[i].quality - 1;
 
         itemDao.saveQuality(items[i]);
     }
