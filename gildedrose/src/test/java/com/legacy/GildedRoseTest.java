afee9b9645a98095c4df866fef1b6497620d7991
Vsevolod Osmachko <vsevolod_osmachko@epam.com>
2019-11-16 07:44:37 +0200

    Second test

--- gildedrose/src/test/java/com/legacy/GildedRoseTest.java
+++ gildedrose/src/test/java/com/legacy/GildedRoseTest.java
@@ -1,21 +1,30 @@
 package com.legacy;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
+    private final Item[] items = new Item[]{new Item("Default", 10, 10)};
+    private GildedRose gildedRose;
+
+    @Before
+    public void setUp() throws Exception {
+        gildedRose = new GildedRose(items);
+        gildedRose.setItemDao(new ItemDaoDummy());
+    }
+
     @Test
     public void shouldDecreaseItemQualityForDefaultItem() {
-        Item[] items = {new Item("Default", 10, 10)};
-        GildedRose gildedRose = new GildedRose(items);
-        gildedRose.setItemDao(new ItemDaoDummy());
         gildedRose.updateQuality();
-
         assertEquals(9, items[0].quality);
     }
 
-
+    @Test
+    public void shouldDecreaseItemSellInnForDefaultItem() {
+        gildedRose.updateQuality();
+        assertEquals(9, items[0].sellIn);
+    }
 }
