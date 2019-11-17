5b58b5490a67272271c790cf1b2ea2a6ff9a1f10
Vsevolod Osmachko <vsevolod_osmachko@epam.com>
2019-11-17 20:30:22 +0200

    Merge nested ifs

--- gildedrose/src/main/java/com/legacy/GildedRose.java
+++ gildedrose/src/main/java/com/legacy/GildedRose.java
@@ -37,10 +37,8 @@ public class GildedRose {
             }
 
 
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    decrease(i);
-                }
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                decrease(i);
             }
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -51,13 +49,9 @@ public class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            decrease(i);
+                if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decrease(i);
 
-                        }
-                    }
                 }
             }
         }
